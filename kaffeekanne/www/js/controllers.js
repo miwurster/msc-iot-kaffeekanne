a0ef65c1bed5ff863eb171967513cb33d339c321
miwurster <miwurster@googlemail.com>
2016-06-07 15:53:35 +0200

    Pretty simple app UI

--- kaffeekanne/www/js/controllers.js
+++ kaffeekanne/www/js/controllers.js
@@ -1,5 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($log, $scope, Coffee) {
 
-;
+  $scope.coffee = {};
+
+  Coffee.all().then(function(response) {
+    $log.debug(JSON.stringify(response.data));
+    $scope.coffee = response.data;
+  }).catch(function(response){
+    $log.error(JSON.stringify(response));
+  });
+});
